7f27292e61f2fe7f7f72a8dab74b1592bab66136
cristalmirror <crisrojari@gmail.com>
2026-02-09 18:54:04 -0300

    add code to update

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
@@ -48,24 +49,38 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         port = (uint16_t) strtoul(argv[1], NULL, 10);
     }
-    char *hello = "HTTP/1.1 200 OK\nContnt-Type: text/plan\nContext-Length: 11\nHola Mundo!!!.";
+    char *hello = "HTTP/1.1 200 OK\r\nContnt-Type: text/plan\r\nContext-Length: 11\r\nHola Mundo!!!.";
 
     init_openssl();
     SSL_CTX *ctx = create_context();
     configure_context(ctx);
-
+    const char *ip_str = argv[2];
 
     //1. create the sockets
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     //2. Congiguration directions and ports
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = inet_addr(ip_str);
     address.sin_port = htons(port);
     
     if (bind(server_fd,(struct sockaddr*) &address, sizeof(address)) < 0) {
-        perror("***Error in bind: Need root permissions to the port 433***");
+        perror("[SERVER]: ***Error in bind: Need root permissions to the port\n***");
         return 1;
+    }
+
+    struct sockaddr_in local_address;
+    socklen_t address_leng = sizeof(local_address);
+
+    if (getsockname(server_fd,(struct sockaddr *) &local_address, &address_leng)) {
+        perror("[SERVER]: ***Error trying take the congigurations socket***\n");
+    } else {
+        char *ip = inet_ntoa(local_address.sin_addr);
+        uint16_t real_port = ntohs(local_address.sin_port);
+        
+        printf("[SERVER]: succesfull init server \n");
+        printf("[SERVER]: IP => %s\n",ip);
+        printf("[SERVER]: PORT => %u\n",real_port);
     }
 
     //3. listen connections
@@ -73,12 +88,13 @@ int main(int argc, char *argv[]) {
     while (1) {
         SSL *ssl;
         //4. Accept Connection
-        web_socket = accept(server_fd,(struct sockaddr*) &address, (socklen_t*) &address);
+        web_socket = accept(server_fd,(struct sockaddr*) &address, &address_leng);
         
         ssl = SSL_new(ctx);
         SSL_set_fd(ssl,web_socket);
         
         if (SSL_accept(ssl) <= 0) {
+            fprintf(stderr, "Fallo en el handshake SSL\n");
             ERR_print_errors_fp(stderr);
         } else {
             SSL_write(ssl, hello, strlen(hello));
